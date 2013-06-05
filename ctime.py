c03c482a4dccd56eb47324bf464c57d4b54dad0a
Carwyn Pelley <cpelley.pub@gmail.com>
2013-06-05 20:25:09 +0100

    working trace on benchmark

--- ctime.py
+++ ctime.py
@@ -1,17 +1,35 @@
+"""Timer class for simple benchmarking"""
+
 import datetime
 import inspect
 
 
 class TimeIt(object):
     def __init__(self, t1=None):
-        """Timming a code snipper."""
+        """
+        Timming a code snippet.
+
+        Kwargs:
+
+        * t1 (datetime object):
+            Start time
+
+        .. note::
+
+            A timedelta object is assigned to self.result and trace
+            information storred in self.location
+
+        """
         if t1:
             self.t1 = t1
         else:
             self.t1 = None
         self.t2 = None
 
-        self.line1 = inspect.stack()[1]
+        frame, fnme, lineno, func_nme, lines, _ = inspect.getouterframes(
+            inspect.currentframe())[1]
+        self.location = '> {}({}){}\n->{}'.format(
+            fnme, lineno, func_nme, lines[0])
 
     def __enter__(self):
         self.t1 = datetime.datetime.now()
@@ -19,4 +37,9 @@ class TimeIt(object):
 
     def __exit__(self, type, value, traceback):
         self.t2 = datetime.datetime.now()
-        print 'completed in {}'.format(self.t2 - self.t1)
+        self.result = self.t2 - self.t1
+        self.scr_print()
+
+    def scr_print(self):
+        print self.location
+        print 'completed in {}\n'.format(self.t2 - self.t1)

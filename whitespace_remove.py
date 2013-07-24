de07eb7a21e9fddc48795d954d01071975f6e361
Carwyn Pelley <cpelley.pub@gmail.com>
2013-07-24 23:06:59 +0100

    ENH: Generalised whitespace rm commandline inter
    This commit generalises the whitespace remover commandline interface
    through use of argparse.  Enabled a useful recursive, diff and debug
    option.

--- whitespace_remove.py
+++ whitespace_remove.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 """remove whitespace/emptylines from specified filetypes"""
-
+import collections
+import argparse
 import os
 import re
 import difflib
 
 
 __author__ = "Carwyn Pelley"
+__copyright__ = "2013, Carwyn Pelley"
 __license__ = "LGPLv3"
 __version__ = "0.9.0"
 __email__ = "cpelley.pub@gmail.com"
@@ -45,13 +47,19 @@ def rm_whitespace(fname):
     return nlines, diff
 
 
-def tree_find(dname=os.getcwd(), ext_filter=LAN_FMT['python']):
+def tree_find(dname, ext_filter, recursive=True):
     # Return all files recursively under specified directory that match filter
     flist = []
-    for root, _, fnames in os.walk(dname):
+    if recursive is not True:
+        root, dirs, fnames = next(os.walk(dname))
         for fname in fnames:
             if os.path.splitext(fname)[1] in ext_filter:
                 flist.append(os.path.join(root, fname))
+    else:
+        for root, dirs, fnames in os.walk(dname):
+            for fname in fnames:
+                if os.path.splitext(fname)[1] in ext_filter:
+                    flist.append(os.path.join(root, fname))
     return flist
 
 
@@ -62,20 +70,61 @@ def update_file(fname, nlines):
 
 
 if __name__ == '__main__':
-    flist = tree_find()
-    debug = False
+    prog_desc = """
+    This program allows whitespace removal at a specified location over a
+    specified list of filetypes.  Filetype matching can also occur recursively
+    if chosen and additionally a debug mode can be chosen which invokes a diff
+    preview of the changes to be made as apposed to making actual changes.
 
+    Default setting's are a non-recursive whitespace removal of python
+    specific filetypes (.py and .pyx).
+    """
+    parser = argparse.ArgumentParser(prog='Whitespace remover',
+                                     description=prog_desc)
+    parser.add_argument('--location', '-l', default=os.getcwd(), type=str,
+                        help=('Location to apply whitespace removal '
+                              '(default: ./)'))
+
+    parser.add_argument('--filter', '-f', default=LAN_FMT['python'],
+                        nargs='+', type=str,
+                        help=('Filter filetypes, '
+                              '(default: {})'.format(LAN_FMT['python'])))
+
+    parser.add_argument('--recursive', '-r', action='store_true',
+                        help=('Turn on/off recursive matching, '
+                              '(default: True)'))
+
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help=('Debug mode does a print out of the files that '
+                              'would change (default: False)'))
+    parser.add_argument('--diff', '-df', action='store_true',
+                        help=('Implements a diff of the changes '
+                              'that will be made without actually making '
+                              'them (default: False)'))
+
+    args = parser.parse_args()
+
+    if args.debug:
+        print '\nchosen arguments: {}'.format(args)
+
+    flist = tree_find(args.location, args.filter, args.recursive)
+
+    updated = collections.Counter()
     for fname in flist:
         nlines, diff = rm_whitespace(fname)
-        if debug:
-            for dd in diff:
-                print dd
-        elif nlines:
-            # Test is generator is empty
-            try:
-                diff.next()
-                print 'updating: {}'.format(fname)
-                update_file(fname, nlines)
-            except StopIteration:
-                pass
-    print 'completed'
+        for dd in diff:
+            if nlines:
+                updated.update([fname])
+                if args.diff is True:
+                    print dd
+                if args.debug is not True:
+                    update_file(fname, nlines)
+                    break
+
+    if args.debug is True:
+        msg = 'file change preview: {}, {} lines difference'
+    else:
+        meg = 'updating: {}, {} lines changed'
+    for fname in updated.iteritems():
+        print msg.format(*fname)
+    print '\ncompleted'
